ca4a7beca20e677350617e4c9c3863959c52c7e1
devcsw <mbso520@gmail.com>
2021-06-06 14:45:46 +0900

    MVC_controller

--- server/data/tweet.js
+++ server/data/tweet.js
@@ -1,55 +1,54 @@
 let tweets = [
     {
-        id: '1',
-        text: "안녕하세요",
-        createdAt: Date.now().toString(),
-        name: 'Bob',
-        username: 'bob',
-        url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
+      id: '1',
+      text: '드림코더분들 화이팅!',
+      createdAt: Date.now().toString(),
+      name: 'Bob',
+      username: 'bob',
+      url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
     },
     {
-        id: '2',
-        text: "안녕하세요",
-        createdAt: Date.now().toString(),
-        name: 'Choi',
-        username: 'choi',
-        url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
+      id: '2',
+      text: '안뇽!',
+      createdAt: Date.now().toString(),
+      name: 'Ellie',
+      username: 'ellie',
     },
-];
-
-export function getAll(){
+  ];
+  
+  export async function getAll() {
     return tweets;
-}
-export function getAllByUsername(username){
+  }
+  
+  export async function getAllByUsername(username) {
     return tweets.filter((tweet) => tweet.username === username);
-}
-
-
-export function getById(id){
+  }
+  
+  export async function getById(id) {
     return tweets.find((tweet) => tweet.id === id);
-}
-
-
-export function create(text, name, username){
+  }
+  
+  export async function create(text, name, username) {
     const tweet = {
-        id: Date.now().toString(),
-        text,
-        createdAt: new Date(),
-        name,
-        username,
+      id: Date.now().toString(),
+      text,
+      createdAt: new Date(),
+      name,
+      username,
     };
     tweets = [tweet, ...tweets];
     return tweet;
-}
-
-export function update(id, text){
+  }
+  
+  export async function update(id, text) {
     const tweet = tweets.find((tweet) => tweet.id === id);
     if (tweet) {
-        tweet.text = text;
+      tweet.text = text;
     }
     return tweet;
-}
-
-export function remove(id) {
+  }
+  
+  export async function remove(id) {
     tweets = tweets.filter((tweet) => tweet.id !== id);
-}
+  }
+  
